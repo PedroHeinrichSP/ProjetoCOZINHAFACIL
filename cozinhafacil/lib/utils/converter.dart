8b40fd50971e98f9ddf6c75f0c24ecce5a79f2fc
HeinrichFeather <103895487+PedroHeinrichSP@users.noreply.github.com>
2023-09-23 14:12:36 -0300

    Update converter.dart

--- cozinhafacil/lib/utils/converter.dart
+++ cozinhafacil/lib/utils/converter.dart
@@ -1,15 +1,66 @@
-Map<String, Map<String, double>> ingredientConversions = {
+Map<String, Map<String, double>> ingredientConversions = { // map ingredient to measure to grams
   'sugar': {
-    'teaspoon to grams': 4.0,
-    'spoon to grams': 12.0,
+    'teaSpoon': 4.0,
+    'dessertSpoon': 8.0,
+    'commonSpoon': 12.0,
+    'cup': 148.0,
+    'teaCup': 180.0,
   },
-  'flour': {
-    'cup to grams': 120.0,
-    'ounce to grams': 28.35,
+  'starch': {
+    'teaSpoon': 3.0,
+    'dessertSpoon': 6.0,
+    'commonSpoon': 8.0,
+    'cup': 105.0,
+    'teaCup': 128.0,
   },
-  'butter': {
-    'tablespoon to grams': 14.18,
-    'cup to grams': 227.0,
+  'rice': {
+    'teaSpoon': 4.0,
+    'dessertSpoon': 8.0,
+    'commonSpoon': 12.0,
+    'cup': 150.0,
+    'teaCup': 185.0,
+  },
+  'yeast': { //equivalent to sodium bicarbonate and both types of yeast
+    'teaSpoon': 5.0,
+    'dessertSpoon': 10.0,
+    'commonSpoon': 14.0,
+    'cup': 184.0,
+    'teaCup': 224.0,
+  },
+  'coffee': {
+    'teaSpoon': 2.0,
+    'dessertSpoon': 4.0,
+    'commonSpoon': 5.0,
+    'cup': 66.0,
+    'teaCup': 80.0,
+  },
+  'nuts': {
+    'teaSpoon': 3.0,
+    'dessertSpoon': 6.0,
+    'commonSpoon': 9.0,
+    'cup': 115.0,
+    'teaCup': 140.0,
+  },
+  'chocolate': {
+    'teaSpoon': 2.0,
+    'dessertSpoon': 4.0,
+    'commonSpoon': 6.0,
+    'cup': 74.0,
+    'teaCup': 90.0,
+  },
+  'coconut': {
+    'teaSpoon': 2.0,
+    'dessertSpoon': 4.0,
+    'commonSpoon': 6.0,
+    'cup': 82.0,
+    'teaCup': 100.0,
+  },
+  'wheat': {
+    'teaSpoon': 2.5,
+    'dessertSpoon': 5.0,
+    'commonSpoon': 7.5,
+    'cup': 98.5,
+    'teaCup': 120.0,
   },
   // TODO Add more ingredients and their conversions as needed
 };
